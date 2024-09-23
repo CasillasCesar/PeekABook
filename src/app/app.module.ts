0b9055a1b1e3c52cf6184b0a079841bf7d07173a
CasillasCesar <cesarcasillascespedes@gmail.com>
2024-09-23 16:48:48 -0600

    feat(general) : Se configura la app para permitir usar PrimeNG
    Se hacen las primeras configuraciones para poder emplear la libreria de componentes llamado primeNG.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimengModule } from './primeng.module'
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],  // Aquí declaras tus componentes
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    PrimengModule,
     AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    ],  // Aquí importas otros módulos necesarios
+  bootstrap: [AppComponent]  // Este es el componente raíz que arranca la aplicación
 })
-export class AppModule { }
+export class AppModule {}
