5cea1c9a321447bed6b849cb6a8e76d2e16ccd1c
CasillasCesar <cesarcasillascespedes@gmail.com>
2024-10-03 07:30:28 -0600

    Falto este

---
+++ src/app/pages/in-home/in-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-in-home',
+  templateUrl: './in-home.component.html',
+  styleUrl: './in-home.component.css'
+})
+export class InHomeComponent {
+
+}
