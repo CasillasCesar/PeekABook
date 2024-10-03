311ff770ca597ddb88ee414ccde388ee5ed4640d
CasillasCesar <cesarcasillascespedes@gmail.com>
2024-10-03 07:59:12 -0600

    Se generarn las primeras rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
+import { InHomeModule } from './pages/in-home/in-home.module';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeModule } from './pages/home/home.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"in-home",
+    loadChildren : ()=>import('./pages/in-home/in-home.module').then(m => m.InHomeModule)
+  },
+  {
+    path:"home",
+    loadChildren : ()=>import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path : '',
+    redirectTo : 'home',
+    pathMatch: 'full'
+  },
+  {
+    path : '**',
+    redirectTo : 'home',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
