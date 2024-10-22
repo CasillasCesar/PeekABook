0562d459237e93f1173bf67990496f2512ccef0a
CasillasCesar <cesarcasillascespedes@gmail.com>
2024-10-22 12:34:58 -0600

    feat(breadcrumbs) : Se genera componente de migas de pan

---
+++ src/app/components/breadcrumb/breadcrumb.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { filter } from 'rxjs';
+
+@Component({
+  selector: 'app-breadcrumb',
+  templateUrl: './breadcrumb.component.html',
+  styleUrl: './breadcrumb.component.css'
+})
+
+export class BreadcrumbComponent implements OnInit {
+  bread_items: MenuItem[] = [];
+  home: MenuItem = { icon: 'pi pi-home', routerLink: '/' };
+
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.bread_items = this.createBreadcrumbs(this.route.root);
+      });
+  }
+
+  private createBreadcrumbs(route: ActivatedRoute, url: string = '', breadcrumbs: MenuItem[] = []): MenuItem[] {
+    const children: ActivatedRoute[] = route.children;
+
+    if (children.length === 0) {
+      return breadcrumbs;
+    }
+
+    const childRoute = children[0];
+    const routeURL: string = childRoute.snapshot.url.map(segment => segment.path).join('/');
+    if (routeURL !== '') {
+      url += `/${routeURL}`;
+    }
+
+    const breadcrumbLabel = childRoute.snapshot.data['breadcrumb'];
+    if (breadcrumbLabel) {
+      breadcrumbs.push({
+        label: breadcrumbLabel,
+        routerLink: url
+      });
+    }
+
+    return this.createBreadcrumbs(childRoute, url, breadcrumbs);
+  }
+}
