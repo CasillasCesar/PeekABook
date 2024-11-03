570ba6ce582ed70f6949052aa3d7711aa69cb273
CasillasCesar <cesarcasillascespedes@gmail.com>
2024-11-03 16:09:51 -0600

    feat(homer): Se genera componente de inicio
    Se genera un componente de inicio para mostrar informacion de los libros disponibles

--- src/app/components/homer/homer.component.ts
+++ src/app/components/homer/homer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BookCard } from '../../models/book-card';
+import { BooksService } from '../../services/books.service';
 
 @Component({
   selector: 'app-homer',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './homer.component.css'
 })
 export class HomerComponent {
-
+  items : BookCard[] = [];
+  images = [
+    "assets\dummies\portada-calavera-dia-de-muertos.webp",
+    "assets\dummies\los-elementos-de-un-altar-de-muertos.jpg",
+    "https://www.caminoreal.com/storage/app/media/Blog/los-elementos-de-un-altar-de-muertos.jpg"
+  ]
+  constructor(private bookService : BooksService){
+    this.items = this.bookService.getDummiBooks()
+  }
 }
