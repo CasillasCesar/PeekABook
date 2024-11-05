ac2499af1541c8edc4d7905b791c73ce3a4e0dd0
CasillasCesar <cesarcasillascespedes@gmail.com>
2024-11-04 20:47:48 -0600

    feat (book-info): Se genera componente de informacion extra
    Se genera un componente para mostrar mas informacion del libro seleccionado

--- src/app/components/book-info/book-info.component.ts
+++ src/app/components/book-info/book-info.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Book } from '../../models/book';
+import { ActivatedRoute, Route } from '@angular/router';
+import { BooksService } from '../../services/books.service';
+import { BookCard } from '../../models/book-card';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-book-info',
@@ -6,5 +11,21 @@ import { Component } from '@angular/core';
   styleUrl: './book-info.component.css'
 })
 export class BookInfoComponent {
+  loged : Boolean = false;
+  book : Book = {} as Book;
+  user : User = {} as User;
+  bookId : number = 0;
+  items : BookCard[] = [];
+  constructor(private bookService : BooksService, private route : ActivatedRoute){
+    this.items = this.bookService.getDummiBooks()
+    this.route.data.subscribe(data=>{
+      this.loged = data['loged']
+    }).unsubscribe()
+    this.route.paramMap.subscribe((param : any)=>{
+      this.bookId = param['params']['id']
+    }).unsubscribe()
+    this.book = this.items[this.bookId]['dataBook']
+    this.user = this.items[this.bookId]['user']
 
+  }
 }
