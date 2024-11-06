2f8b44dfe5026c2820002a94f8cafa020b916b76
CasillasCesar <cesarcasillascespedes@gmail.com>
2024-11-06 08:49:06 -0600

    Se generan documentos para generar pagina de inicio de sesion

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,11 +5,14 @@ import { InHomeComponent } from './in-home/in-home.component';
 import { HomeModule } from './home/home.module';
 import { InHomeModule } from './in-home/in-home.module';
 import { ComponentsModule } from '../components/components.module';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
+  
+    LoginComponent
   ],
   imports: [
     CommonModule,
