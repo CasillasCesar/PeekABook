6f532a8002cc7236c6432a2a554da97178d010ff
CasillasCesar <cesarcasillascespedes@gmail.com>
2024-11-06 13:00:19 -0600

    feat(register) : Generacion de registro
    Se genera el formulario encagado de permitir a los usuarios registrarse en el sitio web.

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.email]],
+      phoneNumber: ['',[Validators.pattern('^[0-9]{10}$')]],
+      state: ['', Validators.required],
+      city: ['', Validators.required],
+      calif: [5] // Valor predeterminado de calificación, sin incluir en el formulario visible
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      const userData = this.registerForm.value;
+      console.log('Registro exitoso:', userData);
+      // Aquí puedes enviar `userData` al servidor
+    }
+  }
 }
