158119c4e48fcaa0aace39ac721ef1785181f78f
CasillasCesar <cesarcasillascespedes@gmail.com>
2024-11-06 13:09:09 -0600

    feat(login.page) : Generacion de login
    Se genera la interfaz para que los usuarios inicien sesion los usuarios

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const loginData = this.loginForm.value;
+      console.log('Datos de inicio de sesión:', loginData);
+      // Aquí puedes llamar a un servicio para autenticar al usuario
+    }
+  }
 }
